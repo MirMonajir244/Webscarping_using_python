a523beff78ada7665cbe031bd7ed99d46a90af1a
mit <mirmonajir2017@gmail.com>
2021-07-11 00:32:07 +0530

    Final commit after completing

--- scarping.py
+++ scarping.py
@@ -1,12 +1,34 @@
-#pip install bs4
-#pip install html5lib
-#pip install requests
+# pip install bs4
+# pip install html5lib
+# pip install requests
 import requests
 from bs4 import BeautifulSoup
-url = "https://ums.lpu.in/"
+url = input("Enter your full website link: ")
 
-#Get The HTML
+# Get The HTML
 
 req = requests.get(url)
 HtmlContent = req.content
-print(HtmlContent)
+# print(HtmlContent)
+
+# Parsing the HtmlContent
+soup = BeautifulSoup(HtmlContent, 'html.parser')
+# print(soup.prettify) #prettify make the HtmlContent's pretty
+
+# Travarse the Soup
+# Get all the paragraph tag
+paragraph = soup.find_all('p')
+print(paragraph)
+
+print('#'*125)
+
+# get all the anchor link
+
+anchor = soup.find_all('a')
+links = set()
+for link in anchor:
+    if(link.get('href') != '#'):
+        textlink = link.get('href')
+        links.add(link)
+        print(textlink)
+# print(anchor)
